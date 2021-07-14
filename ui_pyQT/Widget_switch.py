c96ab0136045452a03cea9c6c09ca2905194a27a
prmanager1337 <74357991+prmanager1337@users.noreply.github.com>
2021-07-14 09:30:07 +0200

    First Working Code

--- ui_pyQT/Widget_switch.py
+++ ui_pyQT/Widget_switch.py
@@ -31,7 +31,6 @@ class TrainSwitch(QWidget):
                                     "min-height: 2.5em;"
                                     "padding: 2px;")
 
-
         self.led.setDisabled(True)  # Make the led non clickable
 
         self.led.on_color_1 = QColor(255, 0, 0)
@@ -49,11 +48,9 @@ class TrainSwitch(QWidget):
 
         if self.button.isChecked(): 
             write_serial_arduino(self.command_routing + "1")
-            #write_i2c_arduino(self.command_routing + "1")
-        # if it is unchecked 
+
         else: 
             write_serial_arduino(self.command_routing + "2")
-            #write_i2c_arduino(self.command_routing + "2")
 
 class quitApp(QWidget):
     def __init__(self, parent):
@@ -62,7 +59,6 @@ class quitApp(QWidget):
         self.button = QPushButton()
         self.button.setCheckable(True)
         self.button.setText("Exit Application")
-
         self.layout.addWidget(self.button)
         self.button.clicked.connect(lambda: self.button_clicked())
 
@@ -74,53 +70,21 @@ class windowLayout(QWidget):
     def __init__(self, parent):
         super(QWidget, self).__init__(parent)
 
+        switch_button = 1
         self.MapLayout = QHBoxLayout()
         self.ButtonLayout = QGridLayout()
-        self.ButtonLayout.setAlignment(Qt.AlignLeft)
+        self.ButtonLayout.setAlignment(Qt.AlignLeft)          
 
-        self.button_1 = TrainSwitch(self, "1", "11")
-        self.button_2 = TrainSwitch(self, "2", "12")
-        self.button_3 = TrainSwitch(self, "3", "13")
-        self.button_4 = TrainSwitch(self, "4", "14")
-        self.button_5 = TrainSwitch(self, "5", "15")
-        self.button_6 = TrainSwitch(self, "6", "16")
-        self.button_7 = TrainSwitch(self, "7", "17")
-        self.button_8 = TrainSwitch(self, "8", "21")
-        self.button_9 = TrainSwitch(self, "9", "22")
-        self.button_10 = TrainSwitch(self, "10", "23")
-        self.button_11 = TrainSwitch(self, "11", "24")
-        self.button_12 = TrainSwitch(self, "12", "25")
-        self.button_13 = TrainSwitch(self, "13", "26")
-        self.button_14 = TrainSwitch(self, "14", "27")
-        self.button_15 = TrainSwitch(self, "15", "31")
-        self.button_16 = TrainSwitch(self, "16", "32")
-        self.button_17 = TrainSwitch(self, "17", "33")
-        self.button_18 = TrainSwitch(self, "18", "34")
-        self.button_19 = TrainSwitch(self, "19", "35")
+        for i in range(0, 8):
+            for j in range(0, 5):
+                self.switch_button =  TrainSwitch(self, str(switch_button), "11")
+                self.ButtonLayout.addLayout(self.switch_button.layout, i, j)
+                switch_button += 1
 
-        self.button_20 = quitApp(self)
-
-        self.ButtonLayout.addLayout(self.button_1.layout, 1, 0)        
-        self.ButtonLayout.addLayout(self.button_2.layout, 1, 1)       
-        self.ButtonLayout.addLayout(self.button_3.layout, 1, 2)
-        self.ButtonLayout.addLayout(self.button_4.layout, 1, 3)
-        self.ButtonLayout.addLayout(self.button_5.layout, 2, 0) 
-        self.ButtonLayout.addLayout(self.button_6.layout, 2, 1) 
-        self.ButtonLayout.addLayout(self.button_7.layout, 2, 2) 
-        self.ButtonLayout.addLayout(self.button_8.layout, 2, 3) 
-        self.ButtonLayout.addLayout(self.button_9.layout, 3, 0) 
-        self.ButtonLayout.addLayout(self.button_10.layout, 3, 1) 
-        self.ButtonLayout.addLayout(self.button_11.layout, 3, 2) 
-        self.ButtonLayout.addLayout(self.button_12.layout, 3, 3) 
-        self.ButtonLayout.addLayout(self.button_13.layout, 4, 0) 
-        self.ButtonLayout.addLayout(self.button_14.layout, 4, 1) 
-        self.ButtonLayout.addLayout(self.button_15.layout, 4, 2) 
-        self.ButtonLayout.addLayout(self.button_16.layout, 4, 3) 
-        self.ButtonLayout.addLayout(self.button_17.layout, 5, 0) 
-        self.ButtonLayout.addLayout(self.button_18.layout, 5, 1) 
-        self.ButtonLayout.addLayout(self.button_19.layout, 5, 2) 
-        self.ButtonLayout.addLayout(self.button_20.layout, 5, 3) 
-
+        self.button_quit = quitApp(self)       
+        self.ButtonLayout.addLayout(self.button_quit.layout, switch_button / 4, j)
 
         self.MapLayout.addLayout(self.ButtonLayout)
-        self.setLayout(self.MapLayout)        
+        self.setLayout(self.MapLayout)  
+
+              
--- ui_pyQT/interface.py
+++ ui_pyQT/interface.py
@@ -31,6 +31,6 @@ class Window(QMainWindow):
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     win = Window()
-    #win.show()
-    win.showFullScreen() 
+    win.show()
+    #win.showFullScreen() 
     sys.exit(app.exec_())
